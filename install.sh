776715866be478a9f99a1e8df8ce93246f8dbd47
keahi32 <50500100+keahi32@users.noreply.github.com>
2023-02-22 11:32:54 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo " "
+echo "-------------------------------"
+echo " "
+echo "Instalacion Automatica de Login."
+echo "Developed by Jesus Dev and g0dsp."
+echo " "
+echo "-------------------------------"
+sleep 5
+echo "-------------------------------"
+echo " "
+echo "Descargando git"
+echo " "
+echo "-------------------------------"
+sudo apt install git &>/dev/null
+git clone https://github.com/keahi32/login
